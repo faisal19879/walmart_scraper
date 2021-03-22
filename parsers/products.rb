6e6b2e48f914e0a585eb71ce987536ff98a9b3dc
physicswala <faisaljamali15@gmail.com>
2021-03-22 21:14:00 +0300

    updating products.rb

--- parsers/products.rb
+++ parsers/products.rb
@@ -4,26 +4,26 @@ nokogiri = Nokogiri.HTML(content)
 product = {}
 
 #extract title
-product['title'] = nokogiri.at_css('.search-result-gridview-items .search-result-product-title').text.strip
+product['title'] = nokogiri.at_css('.prod-ProductTitle').attr('content').strip
 
 #extract current price
 product['current_price'] = nokogiri.at_css('span.price-characteristic').attr('content').to_f
 
 #extract original price
-original_price_div = nokogiri.at_css('.price .visuallyhidden')
+original_price_div = nokogiri.at_css('.price-old')
 original_price = original_price_div ? original_price_div.text.strip.gsub('$','').to_f : nil
 product['original_price'] = original_price == 0.0 ? nil : original_price
 
 #extract rating
-rating = nokogiri.at_css('.hiddenStarLabel .seo-avg-rating').text.strip.to_f
+rating = nokogiri.at_css('.button-wrapper span').text.strip.to_f
 product['rating'] = rating == 0 ? nil : rating
 
 #extract number of reviews
-review_text = nokogiri.at_css('.stars-reviews-count-node').text.strip
-product['reviews_count'] = review_text =~ /reviews/ ? review_text.split(' ').first.to_i : 0
+review_text = nokogiri.at_css('seo-review-count').text.strip
+product['reviews_count'] = review_text
 
 #extract publisher
-product['publisher'] = nokogiri.at_css('a.prod-brandName').text.strip
+product['publisher'] = nokogiri.at_css('a.prod-brandName')[4].text.strip
 
 #extract walmart item number
 product['walmart_number'] = nokogiri.at_css('.prod-productsecondaryinformation .wm-item-number').text.split('#').last.strip
