b6013a9821c21cd80d148662b0d9f9e2e27a9663
physicswala <faisaljamali15@gmail.com>
2021-03-23 21:30:15 +0300

    updating products.rb and config.yaml and adding exporter

--- parsers/products.rb
+++ parsers/products.rb
@@ -7,32 +7,29 @@ product = {}
 product['title'] = nokogiri.at_css('.prod-ProductTitle').attr('content').strip
 
 #extract current price
-product['current_price'] = nokogiri.at_css('span.price-characteristic').attr('content').to_f
+product['current_price'] = nokogiri.at_css('.price-characteristic').attr('content').to_f
 
 #extract original price
-original_price_div = nokogiri.at_css('.price-old')
-original_price = original_price_div ? original_price_div.text.strip.gsub('$','').to_f : nil
-product['original_price'] = original_price == 0.0 ? nil : original_price
+original_price_div = nokogiri.at_css('.price .visuallyhidden')
+original_price = original_price_div ? original_price_div.to_s.match(/\d+.\d+/) : nil
+product['original_price'] = original_price == '0.0' ? nil : original_price
 
 #extract rating
-rating = nokogiri.at_css('.button-wrapper span').text.strip.to_f
-product['rating'] = rating == 0 ? nil : rating
+rating = nokogiri.at_css('.stars-container').attr('aria-label').match(/^\d*/)
+product['rating'] = rating == "0" ? nil : rating
 
 #extract number of reviews
-review_text = nokogiri.at_css('seo-review-count').text.strip
-product['reviews_count'] = review_text
+ratenum = nokogiri.at_css('.stars-reviews-count-node')
+product['reviews_count'] = ratenum.to_s.split.last == "ratings" ? ratenum.to_s.split.first : "0"
 
 #extract publisher
-product['publisher'] = nokogiri.at_css('a.prod-brandName')[4].text.strip
+product['publisher'] = nokogiri.at_css('a.prod-brandName span').to_s.strip
 
 #extract walmart item number
-product['walmart_number'] = nokogiri.at_css('.prod-productsecondaryinformation .wm-item-number').text.split('#').last.strip
+product['walmart_number'] = nokogiri.at_css('.valign-middle.secondary-info-margin-right').text.split('#').last.strip
 
 #extract product image
-product['img_url'] = nokogiri.at_css('.prod-hero-image img')['src'].split('?').first
-
-#extract product categories
-product['categories'] = nokogiri.css('.breadcrumb-list li').collect{|li| li.text.strip.gsub('/','') }
+product['img_url'] = "https:"+nokogiri.at_css('.prod-hero-image-image').attr("src").split('?').first
 
 # specify the collection where this record will be stored
 product['_collection'] = 'products'
